6470ab672138086f872552264561c0e4bcbfb4b1
Alejandro Matute <alexghoro@outlook.com>
2018-08-30 13:36:51 -0400

    Add currenies languages and translations pages

--- MemeSounds/MemeSounds/ViewModels/LandViewModel.cs
+++ MemeSounds/MemeSounds/ViewModels/LandViewModel.cs
@@ -8,19 +8,35 @@ namespace MemeSounds.ViewModels
   public class LandViewModel : BaseViewModel
   {
     private ObservableCollection<Border> borders;
-    
+    private ObservableCollection<Currency> currencies;
+    private ObservableCollection<Language> languages;
+
     public Land Land { get; set; }
 
     public ObservableCollection<Border> Borders
     {
-      get { return this.borders; }
-      set { this.SetValue(ref this.borders, value); }
+      get => borders;
+      set => SetValue(ref borders, value);
+    }
+
+    public ObservableCollection<Currency> Currencies
+    {
+      get => currencies;
+      set => SetValue(ref currencies, value);
+    }
+
+    public ObservableCollection<Language> Languages
+    {
+      get => languages;
+      set => SetValue(ref languages, value);
     }
 
     public LandViewModel(Land land)
     {
       Land = land;
       LoadBorders();
+      Currencies = new ObservableCollection<Currency>(Land.Currencies);
+      Languages = new ObservableCollection<Language>(Land.Languages);
     }
 
     private void LoadBorders()
