3b188a21a918cb659d54871b394b15e2e62cfed1
Alejandro Matute <alexghoro@outlook.com>
2018-09-05 14:44:24 -0400

    Add Api project to solution

---
+++ MemeSounds.API/Startup.cs
@@ -0,0 +1,53 @@
+﻿using MemeSounds.API.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data.SqlClient;
+
+namespace MemeSounds.API
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+      var DbConnectionString = BuildDBConnectionString();
+      services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(DbConnectionString));
+    }
+
+    private string BuildDBConnectionString()
+    {
+      var builder = new SqlConnectionStringBuilder(Configuration.GetConnectionString("AzureConnection"))
+      {
+        Password = Configuration["DbPassword"]
+      };
+      return builder.ConnectionString;
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseHsts();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseMvc();
+    }
+  }
+}
---
+++ MemeSounds.Backend/Startup.cs
@@ -0,0 +1,77 @@
+﻿using MemeSounds.Backend.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data.SqlClient;
+
+namespace MemeSounds.Backend
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.Configure<CookiePolicyOptions>(options =>
+      {
+        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+        options.CheckConsentNeeded = context => true;
+        options.MinimumSameSitePolicy = SameSiteMode.None;
+      });
+
+      var DbConnectionString = BuildDBConnectionString();
+      services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(DbConnectionString));
+
+      services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseDatabaseErrorPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+      app.UseCookiePolicy();
+
+      app.UseAuthentication();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+          name: "default",
+          template: "{controller=Home}/{action=Index}/{id?}");
+      });
+    }
+
+    private string BuildDBConnectionString()
+    {
+      var builder = new SqlConnectionStringBuilder(Configuration.GetConnectionString("AzureConnection"))
+      {
+        Password = Configuration["DbPassword"]
+      };
+      return builder.ConnectionString;
+    }
+  }
+}
